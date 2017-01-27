9e20e47fb7357305fcdb28525a7f157f8960d92f
wang stephen <551619855@qq.com>
2017-01-27 20:42:32 +0800

    background service for accept and pre-process data.

--- app/src/main/java/com/example/steve/plottest/DataServer.java
+++ app/src/main/java/com/example/steve/plottest/DataServer.java
@@ -3,8 +3,28 @@ package com.example.steve.plottest;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.logging.SocketHandler;
+import java.net.Socket;
+import java.net.ServerSocket;
 
 public class DataServer extends Service {
+
+    private static final int PORT = 21994;
+
+    private List<Socket> mList = new ArrayList<Socket>();
+    private ServerSocket server = null;
+    private Socket socket = null;
+    private ExecutorService mExecutorService = null; // thread pool;
+
+
+    private static final String TAG = "DataServer";
+
+//    SocketHandler
     public DataServer() {
     }
 
@@ -12,5 +32,35 @@ public class DataServer extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+//        return super.onStartCommand(intent, flags, startId);
+
+        return 0;
+    }
+
+    private Boolean StartServer()
+    {
+        try{
+
+
+        }catch (Exception e)
+        {
+            Log.d(TAG, "StartServer: "+e.getMessage());
+        }
+
+        return true;
     }
 }
